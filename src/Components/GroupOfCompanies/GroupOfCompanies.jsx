c60d1ccee7c0447659f071eefae2223c5a9acadd
Jazeel Zainudeen <zainudheenjazeel@gmail.com>
2024-07-07 08:34:35 +0530

    Started group of companies fix

--- src/Components/GroupOfCompanies/GroupOfCompanies.jsx
+++ src/Components/GroupOfCompanies/GroupOfCompanies.jsx
@@ -1,16 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from 'gsap';
 
-import Marquee from 'react-fast-marquee';
-
-import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
-
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Navbar from "../Navbar";
 import Footer from "../Footer";
-import brandImg1 from './../../assets/brands/color/image 43.png';
-import brandImg2 from './../../assets/brands/color/image 44.png';
-import brandImg3 from './../../assets/brands/color/image 46.png';
-import brandImg4 from './../../assets/brands/color/Rectangle 24.png';
-import brandImg5 from './../../assets/brands/color/Rectangle 45.png';
 
 import groupImg1 from './../../assets/group/1.png';
 import groupImg2 from './../../assets/group/2.png';
@@ -21,17 +14,36 @@ import groupImg6 from './../../assets/group/6.png';
 
 import Image1 from "../../assets/a350f6cd3b16d8ad3bc39a824b1e7d82.png";
 import BackgroundImage from "../../assets/57af26e48eb6728309d8a752a133493e.png";
+import GroupSection from "./GroupSection";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const GroupOfCompanies = () => {
+    useEffect(() => {
+        gsap.timeline({ duration: 0.5, ease: 'power3.out' })
+            .fromTo('.section-1 .text-1', { y: 50, opacity: 0 }, { y: 0, opacity: 1 })
+            .fromTo('.section-1 .text-2', { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '.section-2',
+                start: 'top bottom',
+            }
+        })
+            .fromTo('.section-2 .img-1', { x: 150, opacity: 0 }, { x: 0, opacity: 1, duration: 0.6, ease: 'power3.out' })
+            .fromTo('.section-2 .head-1', { x: -50, opacity: 0 }, { x: 0, opacity: 1, duration: 0.6, ease: 'power3.out' })
+            .fromTo('.section-2 .text-1', { x: -50, opacity: 0 }, { x: 0, opacity: 1, duration: 0.6, ease: 'power3.out' });
+    });
+
     return (
         <>
             <Navbar />
-            <div className="h-screen w-screen relative">
+            <div className="h-screen w-screen relative section-1">
                 <img src={BackgroundImage} alt="" className="object-cover object-center h-screen w-screen brightness-50" />
 
                 <div className="absolute top-1/2 left-[3%] -translate-y-1/2 z-10 text-white">
-                    <div className="text-lg md:text-3xl font-semibold mb-4 uppercase">ELITE GROUP HOLDING</div>
-                    <div className="text-4xl md:text-6xl font-extrabold mb-4 uppercase">OUR GROUP of COMPANIES</div>
+                    <div className="text-lg md:text-xl font-medium mb-4 uppercase text-1">ELITE GROUP HOLDING</div>
+                    <div className="text-4xl md:text-6xl font-extrabold mb-4 uppercase text-2">OUR GROUP of COMPANIES</div>
                 </div>
             </div>
 
@@ -43,19 +55,19 @@ const GroupOfCompanies = () => {
                         </div>
                     </div>
 
-                    <div className="py-5 md:py-10 flex max-md:flex-col-reverse max-md:gap-3 sm:flex-row-reverse sm:items-center">
+                    <div className="py-5 md:py-10 flex max-md:flex-col-reverse max-md:gap-3 sm:flex-row-reverse sm:items-center section-2">
                         <div className="relative sm:w-1/2 sm:grow sm:shrink-0 sm:-me-[3.5%] overflow-hidden">
-                            <img src={Image1} alt="" className="w-full sm:h-[20rem] md:h-[26rem] object-cover" />
+                            <img src={Image1} alt="" className="w-full sm:h-[20rem] md:h-[26rem] object-cover img-1" />
                         </div>
                         <div className="xl:px-24 sm:shrink-1 sm:w-1/2">
-                            <div className="text-lg md:text-4xl font-semibold mb-2 md:mb-4">ELITE GROUP HOLDING</div>
-                            <p className="mb-2 md:mb-4 text-[#404040] text-xs md:text-sm">
+                            <div className="text-lg md:text-4xl font-semibold mb-2 md:mb-4 head-1">ELITE GROUP HOLDING</div>
+                            <p className="mb-2 md:mb-4 text-[#404040] text-xs md:text-sm text-1">
                                 Elite Group Holding  is pleased to assist you with an attentive multi-channel service:
                             </p>
-                            <p className="mb-2 md:mb-4 text-[#404040] text-xs md:text-sm">
+                            <p className="mb-2 md:mb-4 text-[#404040] text-xs md:text-sm text-1">
                                 You can ask your questions to the Elite Group Holding  Assistant, who will guide you through the Elite Group Holding.
                             </p>
-                            <p className="mb-2 md:mb-4 text-[#404040] text-xs md:text-sm">
+                            <p className="mb-2 md:mb-4 text-[#404040] text-xs md:text-sm text-1">
                                 If our Assistant is unable to find what you are looking for, please scroll down and get in touch with our dedicated Customer Contact Center via phone or contact form.
                             </p>
                         </div>
@@ -64,80 +76,59 @@ const GroupOfCompanies = () => {
             </div>
 
             <div className="overflow-hidden max-md:px-[3%]">
-                <div className="flex max-md:flex-col-reverse max-md:gap-3 md:flex-row sm:items-center max-md:my-10">
-                    <div className="relative sm:w-[56%] sm:grow sm:shrink-0 sm:-me-[3.5%] overflow-hidden z-10">
-                        <div className="hidden sm:block absolute bg-white max-md:w-1/3 w-1/4 h-[120%] lg:-rotate-[20deg] md:-rotate-[15deg] -top-16 xl:-right-[14.35%] lg:-right-[11%] md:-right-[11%] sm:-right-[14%] "></div>
-                        <img src={Image1} alt="" className="w-full sm:h-[20rem] md:h-[26rem] object-cover" />
-                    </div>
-                    <div className="md:px-12 lg:px-20 xl:px-24 sm:shrink-1 sm:w-1/2">
-                        <div className="text-md sm:text-base lg:text-4xl font-semibold mb-2 md:mb-4">ELITE AUTOMOBILES</div>
-                        <p className="mb-2 md:mb-4 text-[#404040] text-xs lg:text-sm">
-                            Elite Group Holding  is pleased to assist you with an attentive multi-channel service:
-                        </p>
-                        <p className="mb-2 md:mb-4 text-[#404040] text-xs lg:text-sm">
-                            You can ask your questions to the Elite Group Holding  Assistant, who will guide you through the Elite Group Holding.
-                        </p>
-                        <button className="bg-black hover:bg-[#fb511e] text-white transition-all border border-1 border-black hover:border-[#fb511e] rounded-full px-10 py-3 button-1 font-light mb-2 md:mb-8">
-                            Read More
-                            <ArrowLongRightIcon className="ms-4 inline w-6 h-6" />
-                        </button>
-                        <Marquee loop={0} className='gap-5 pb-16 overflow-x-visible brands'>
-                            <img src={brandImg1} className="h-8 px-5" alt="" />
-                            <img src={brandImg2} className="h-8 px-5" alt="" />
-                            <img src={brandImg3} className="h-8 px-5" alt="" />
-                            <img src={brandImg4} className="h-8 px-5" alt="" />
-                            <img src={brandImg5} className="h-8 px-5" alt="" />
-                        </Marquee>
-                    </div>
-                </div>
+                <GroupSection
+                    image={groupImg1}
+                    title="ELITE AUTOMOBILES"
+                    descriptions={[
+                        "Elite Group Holding  is pleased to assist you with an attentive multi-channel service:",
+                        "You can ask your questions to the Elite Group Holding  Assistant who will guide you through the Elite Group Holding."
+                    ]}
+                    hasBrands={true} />
 
-                <div className="flex max-md:flex-col-reverse max-md:gap-3 md:flex-row-reverse sm:items-center max-md:my-10">
-                    <div className="relative sm:w-[56%] sm:grow sm:shrink-0 sm:-me-[3.5%] overflow-hidden">
-                        <div className="hidden sm:block absolute bg-white max-md:w-1/3 w-1/4 h-[120%] rotate-[9deg] lg:rotate-[11deg] xl:rotate-[15deg] -top-10 xl:-left-[18%] lg:-left-[17%] md:-left-[18%] sm:-left-[14%] "></div>
-                        <img src={Image1} alt="" className="w-full sm:h-[20rem] md:h-[26rem] object-cover" />
-                    </div>
-                    <div className="md:px-12 lg:px-20 xl:px-24 sm:shrink-1 sm:w-1/2">
-                        <div className="text-md sm:text-base lg:text-4xl font-semibold mb-2 md:mb-4">ELITE INVESTMENT COMPANY</div>
-                        <p className="mb-2 md:mb-4 text-[#404040] text-xs lg:text-sm">
-                            Elite Group Holding  is pleased to assist you with an attentive multi-channel service:
-                        </p>
-                        <p className="mb-2 md:mb-8 text-[#404040] text-xs lg:text-sm">
-                            You can ask your questions to the Elite Group Holding  Assistant, who will guide you through the Elite Group Holding.
-                        </p>
-                        <button className="bg-black hover:bg-[#fb511e] text-white transition-all border border-1 border-black hover:border-[#fb511e] rounded-full px-10 py-3 button-1 font-light mb-2 md:mb-8">
-                            Read More
-                            <ArrowLongRightIcon className="ms-4 inline w-6 h-6" />
-                        </button>
-                    </div>
-                </div>
+                <GroupSection
+                    image={groupImg2}
+                    title="ELITE INVESTMENT COMPANY"
+                    descriptions={[
+                        "Elite Group Holding  is pleased to assist you with an attentive multi-channel service:",
+                        "You can ask your questions to the Elite Group Holding  Assistant, who will guide you through the Elite Group Holding."
+                    ]}
+                    direction="left" />
 
-                <div className="flex max-md:flex-col-reverse max-md:gap-3 md:flex-row sm:items-center max-md:my-10">
-                    <div className="relative sm:w-[56%] sm:grow sm:shrink-0 sm:-me-[3.5%] overflow-hidden z-10">
-                        <div className="hidden sm:block absolute bg-white max-md:w-1/3 w-1/4 h-[120%] lg:-rotate-[20deg] md:-rotate-[15deg] -top-16 xl:-right-[14.35%] lg:-right-[11%] md:-right-[11%] sm:-right-[14%] "></div>
-                        <img src={Image1} alt="" className="w-full sm:h-[20rem] md:h-[26rem] object-cover" />
-                    </div>
-                    <div className="md:px-12 lg:px-20 xl:px-24 sm:shrink-1 sm:w-1/2">
-                        <div className="text-md sm:text-base lg:text-4xl font-semibold mb-2 md:mb-4">ELITE AUTOMOBILES</div>
-                        <p className="mb-2 md:mb-4 text-[#404040] text-xs lg:text-sm">
-                            Elite Group Holding  is pleased to assist you with an attentive multi-channel service:
-                        </p>
-                        <p className="mb-2 md:mb-4 text-[#404040] text-xs lg:text-sm">
-                            You can ask your questions to the Elite Group Holding  Assistant, who will guide you through the Elite Group Holding.
-                        </p>
-                        <button className="bg-black hover:bg-[#fb511e] text-white transition-all border border-1 border-black hover:border-[#fb511e] rounded-full px-10 py-3 button-1 font-light mb-2 md:mb-8">
-                            Read More
-                            <ArrowLongRightIcon className="ms-4 inline w-6 h-6" />
-                        </button>
-                        <Marquee loop={0} className='gap-5 pb-16 overflow-x-visible brands'>
-                            <img src={brandImg1} className="h-8 px-5" alt="" />
-                            <img src={brandImg2} className="h-8 px-5" alt="" />
-                            <img src={brandImg3} className="h-8 px-5" alt="" />
-                            <img src={brandImg4} className="h-8 px-5" alt="" />
-                            <img src={brandImg5} className="h-8 px-5" alt="" />
-                        </Marquee>
-                    </div>
-                </div>
+                <GroupSection
+                    image={groupImg3}
+                    title="ELITE REAL ESTATE COMPANY"
+                    descriptions={[
+                        "Elite Group Holding  is pleased to assist you with an attentive multi-channel service:",
+                        "You can ask your questions to the Elite Group Holding  Assistant who will guide you through the Elite Group Holding."
+                    ]} />
+
+                <GroupSection
+                    image={groupImg4}
+                    title="ELITE HEALTHCARE COMPANY"
+                    descriptions={[
+                        "Elite Group Holding  is pleased to assist you with an attentive multi-channel service:",
+                        "You can ask your questions to the Elite Group Holding  Assistant, who will guide you through the Elite Group Holding."
+                    ]}
+                    direction="left" />
+
+                <GroupSection
+                    image={groupImg5}
+                    title="ELITE EDUCATION COMPANY"
+                    descriptions={[
+                        "Elite Group Holding  is pleased to assist you with an attentive multi-channel service:",
+                        "You can ask your questions to the Elite Group Holding  Assistant who will guide you through the Elite Group Holding."
+                    ]} />
+
+                <GroupSection
+                    image={groupImg6}
+                    title="ELITE E COMMERCE COMPANY"
+                    descriptions={[
+                        "Elite Group Holding  is pleased to assist you with an attentive multi-channel service:",
+                        "You can ask your questions to the Elite Group Holding  Assistant, who will guide you through the Elite Group Holding."
+                    ]}
+                    direction="left" />
             </div>
+
             <Footer />
         </>
     );
--- src/Components/GroupOfCompanies/GroupSection.jsx
+++ src/Components/GroupOfCompanies/GroupSection.jsx
@@ -1,4 +1,4 @@
-import React from react;
+import React from 'react';
 
 import Marquee from 'react-fast-marquee';
 
@@ -10,29 +10,32 @@ import brandImg3 from './../../assets/brands/color/image 46.png';
 import brandImg4 from './../../assets/brands/color/Rectangle 24.png';
 import brandImg5 from './../../assets/brands/color/Rectangle 45.png';
 
-const GroupSection = ({ image, title, descriptions, url = '#', hasBrands = false }) => {
+const GroupSection = ({ image, title, descriptions, url = '#', hasBrands = false, direction = "right" }) => {
     return (
         <>
-            <div className="overflow-hidden max-md:px-[3%]">
-                <div className="flex max-md:flex-col-reverse max-md:gap-3 md:flex-row sm:items-center max-md:my-10">
-                    <div className="relative sm:w-[56%] sm:grow sm:shrink-0 sm:-me-[3.5%] overflow-hidden z-10">
-                        <div className="hidden sm:block absolute bg-white max-md:w-1/3 w-1/4 h-[120%] lg:-rotate-[20deg] md:-rotate-[15deg] -top-16 xl:-right-[14.35%] lg:-right-[11%] md:-right-[11%] sm:-right-[14%] "></div>
+            <div className="overflow-hidden">
+                <div className={`flex max-md:flex-col-reverse max-md:gap-3 sm:items-center max-md:my-10 ${direction == "right" ? 'md:flex-row' : 'md:flex-row-reverse'}`}>
+                    <div className="relative sm:w-1/2 overflow-hidden z-10">
+                        {/* <div className="hidden sm:block absolute bg-white max-md:w-1/3 w-1/4 h-[120%] lg:-rotate-[20deg] md:-rotate-[15deg] -top-16 xl:-right-[14.35%] lg:-right-[11%] md:-right-[11%] sm:-right-[14%] "></div> */}
                         <img src={image} alt="" className="w-full sm:h-[20rem] md:h-[26rem] object-cover" />
                     </div>
-                    <div className="md:px-12 lg:px-20 xl:px-24 sm:shrink-1 sm:w-1/2">
+                    <div className="md:px-12 lg:px-20 xl:px-24 sm:w-1/2">
                         <div className="text-md sm:text-base lg:text-4xl font-semibold mb-2 md:mb-4">{title}</div>
                         {descriptions.map((description, index) => (
-                            <p className="mb-2 md:mb-4 text-[#404040] text-xs lg:text-sm">
+                            <p key={index} className="mb-2 md:mb-4 text-[#404040] text-xs lg:text-sm">
                                 {description}
                             </p>
                         ))}
-                        <a href={url} className="bg-black hover:bg-[#fb511e] text-white transition-all border border-1 border-black hover:border-[#fb511e] rounded-full px-10 py-3 button-1 font-light mb-2 md:mb-8">
+
+                        <a href={url}
+                            className={`inline-block bg-black hover:bg-[#fb511e] text-white transition-all border border-1 border-black hover:border-[#fb511e] rounded-full px-10 py-3 button-1 font-light mb-2 md:mb-8 ${!hasBrands ? 'mt-6' : ''}`}
+                        >
                             Read More
                             <ArrowLongRightIcon className="ms-4 inline w-6 h-6" />
                         </a>
 
-                        {!hasBrands && (
-                            <Marquee loop={0} className='gap-5 pb-16 overflow-x-visible brands'>
+                        {hasBrands && (
+                            <Marquee loop={0} className='gap-5 overflow-x-visible brands'>
                                 <img src={brandImg1} className="h-8 px-5" alt="" />
                                 <img src={brandImg2} className="h-8 px-5" alt="" />
                                 <img src={brandImg3} className="h-8 px-5" alt="" />
